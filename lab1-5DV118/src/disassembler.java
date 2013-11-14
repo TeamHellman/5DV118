2b8f364a6401f912da5e58c9ad43c63b010fcdf7
c11log <c11log@osthyvel.cs.umu.se>
2013-11-14 15:13:17 +0100

    aaaa

--- lab1-5DV118/src/disassembler.java
+++ lab1-5DV118/src/disassembler.java
@@ -29,9 +29,25 @@ public class disassembler {
      opCode.put(0x16, "blezl");
      opCode.put(0x17, "bgtzl");
 
+     HashMap<Integer, String> opCode1  = new HashMap<Integer, String>();
+     opCode1.put(0, "madd");
+     opCode1.put(1, "maddu");
+     opCode1.put(2, "mul");
+     opCode1.put(4, "msubu");
+     opCode1.put(33, "clz");
+     opCode1.put(34, "clo");
 
+     String value = "null";
      int opcode= Integer.parseInt(binary.substring(0, 6), 2);
-     String value = opCode.get(opcode);
+     if(opcode == 0 || opcode == 28){
+	 int id= Integer.parseInt(binary.substring(27, 32), 2);
+	 System.err.println("id = " + id + "\n" );
+	 value = opCode1.get(id);
+     }
+     else{
+	 value = opCode.get(opcode);
+     }
+
      System.err.println(opcode + " = " + value);
 
  }
