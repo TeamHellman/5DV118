eb8340d4a7217dd62b1e57e0b88abb58e396a6a6
oi11hms <oi11hms@kniv.cs.umu.se>
2013-11-14 14:45:09 +0100

    king

--- lab1-5DV118/src/lab1.java
+++ lab1-5DV118/src/lab1.java
@@ -2,10 +2,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 
 public class lab1 {
 
+	static String hexToBin(String s) {
+		  return new BigInteger(s, 16).toString(2);
+	}
     /**
      * @param args
      */
@@ -18,8 +24,21 @@ public class lab1 {
 	                reader = new BufferedReader(new FileReader(file));
 	                String strLine;
 	                while ((strLine = reader.readLine()) != null) {
-
-	                 System.out.println(strLine);
+	                	ArrayList<String> hexVal = new ArrayList<String>();
+	                	hexVal.add(strLine);
+	                	if(hexVal.get(0).substring(0, 2).equalsIgnoreCase("0x")) {
+	                		String hex = hexVal.get(0).substring(2);
+	                		Long intValue = Long.parseLong(hex, 16);
+	                		Long dividedByBig = intValue/(int)Math.pow(2, 26);
+	                		String hexe = Long.toHexString(dividedByBig);
+	                		System.out.println(hexVal.get(0).substring(2));
+	                		System.out.println(hexe);
+	                		String binary = hexToBin(hex);
+	                		while(binary.length() < 32) {
+	                			binary = "0" + binary;
+	                		}
+	                		System.out.println(binary);
+	                	}
 
 	                }
 	                reader.close();
