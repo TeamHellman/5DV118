d00f4175d1959a53491032d1c0cf13d21888c042
c11log <c11log@osthyvel.cs.umu.se>
2013-11-14 13:43:02 +0100

    first

--- lab1-5DV118/src/lab1.java
+++ lab1-5DV118/src/lab1.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class lab1 {
 
@@ -5,7 +10,23 @@ public class lab1 {
      * @param args
      */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	       BufferedReader reader;
+	        // Read Feeds File
+	        try {
+	            File file = new File(args[0]);
+	            if (file.exists()) {
+	                reader = new BufferedReader(new FileReader(file));
+	                String strLine;
+	                while ((strLine = reader.readLine()) != null) {
+
+	                 System.out.println(strLine);
+
+	                }
+	                reader.close();
+	            }
+	        } catch (IOException e) {
+	            System.err.println("Error: " + e.getMessage());
+	        }
 
     }
 
