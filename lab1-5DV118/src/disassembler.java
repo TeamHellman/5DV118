ecb05bfceae3ceae9e15cc25ce758fa468e89505
hiltan <bmarcus.hellman@gmail.com>
2013-11-28 16:48:44 +0100

    bort med short in med int

--- lab1-5DV118/src/disassembler.java
+++ lab1-5DV118/src/disassembler.java
@@ -76,7 +76,7 @@ public class disassembler {
 			String rsS = mapping.register.get(rsT);
 			String rtS = mapping.register.get(rtT);
 			if (func.getType() == 'r') {
-				short rdT = (short)Integer.parseInt(binary.substring(16, 21), 2);
+				int rdT = (int)Integer.parseInt(binary.substring(16, 21), 2);
 				String rdS = mapping.register.get(rdT);
 				mnemonicFormat = value + " " +  rdS + " " + rsS + " " + rtS;
 			} else if (func.getType() == 'i'){
