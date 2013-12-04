f8cb3bbb4f0eddd12432795e83d6ca0abd096767
hiltan <bmarcus.hellman@gmail.com>
2013-12-04 15:54:32 +0100

    added some comments.

--- lab1-5DV118/src/opCode.java
+++ lab1-5DV118/src/opCode.java
@@ -7,14 +7,29 @@
 public class opCode {
     String opcode;
     char format = ' ';
-
+    
+    /**
+     * Creates a new opcode-object with a opcode and a format. 
+     * 
+     * @param opcode
+     * @param format
+     */
     public opCode(String opcode, char format){
 	this.format=format;
 	this.opcode=opcode;
     }
+    /**
+     * Returns the opcode. 
+     * @return
+     */
     public String getOpcode(){
 	return opcode;
     }
+    
+    /**
+     * Returns the format.
+     * @return
+     */
     public char getType(){
 	return format;
     }
